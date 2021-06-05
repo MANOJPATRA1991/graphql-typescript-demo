30143a10981fc35f001122c9be3ad7bb6c4eca27
Manoj Kumar Patra <patra.manoj0@gmail.com>
2021-06-05 22:40:21 +0530

    Create error constants

---
+++ server/src/enums/error.ts
@@ -0,0 +1,6 @@
+export enum ERRORS {
+  NO_MATCHING_USER = 'No matching user found',
+  INVALID_CREDENTIALS = 'Invalid credentials',
+  INVALID_SIGNUP_DATA = 'You must provide an email and a password',
+  EMAIL_IN_USE = 'Email in use',
+};

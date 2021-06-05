401072ce3b49752853bba2b8f02598a829e68721
Manoj Kumar Patra <patra.manoj0@gmail.com>
2021-06-05 22:48:38 +0530

    [client] App: add authentication logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useQuery } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import './App.css';
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+import { GET_CURRENT_USER } from './graphql/queries/user';
+import { User } from './graphql/types/user';
 
-function App() {
+export const App = () => {
+  const [isAuthenticated, setAuthentication] = useState<boolean>(false);
+  const { loading, error, data, refetch } = useQuery<User>(GET_CURRENT_USER);
+
+  useEffect(() => {
+    if (!loading && error) {
+      setAuthentication(false);
+    }
+
+    if (!loading && data) {
+      setAuthentication(true);
+    }
+  }, [error, data]);
+
+  const refetchUser = async () => {
+    try {
+      await refetch();
+      setAuthentication(true);
+    } catch (e) {
+      setAuthentication(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    !loading ? <div className="container">
+      <Header isAuthenticated={isAuthenticated} refetchUser={refetchUser} />
+      <Main isAuthenticated={isAuthenticated} />
+    </div> : null
   );
-}
-
-export default App;
+};
